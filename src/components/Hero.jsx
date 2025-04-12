6b1cecec2e6286f37ce71d9c7560a0d79761c9a6
Wong Meng Xian <mengxian98@gmail.com>
2025-04-12 17:14:59 +0800

    feat: Hero - Add Text Content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Button from "./shared/Button";
 
 const Hero = () => {
   const [currentVideoIndex, setCurrentVideoIndex] = useState(1);
@@ -12,7 +13,7 @@ const Hero = () => {
 
   return (
     <section>
-      <div className="relative h-screen w-full cursor-pointer">
+      <div className="relative h-screen w-full">
         <video
           loop
           autoPlay
@@ -21,7 +22,7 @@ const Hero = () => {
         />
 
         <div
-          className="absolute-center mask-clip-path rounded-lg scale-50 opacity-0 hover:scale-100 hover:opacity-100 transition-all duration-500 ease-in overflow-hidden z-50"
+          className="absolute-center mask-clip-path cursor-pointer rounded-lg scale-50 opacity-0 hover:scale-100 hover:opacity-100 transition-all duration-500 ease-in overflow-hidden z-50"
           onClick={handleVideoPreviewClick}
         >
           <video
@@ -30,6 +31,24 @@ const Hero = () => {
             className="video-center scale-150 size-64"
             src={`/videos/hero-${nextVideoIndex}.mp4`}
           />
+        </div>
+
+        <div className="absolute top-0 left-0 px-12 mt-28">
+          <h1 className="hero-heading special-font text-blue-100">
+            Redefi<b>n</b>e
+          </h1>
+          <p className="text-md text-blue-100 mb-5">
+            Enter the Metagame. Defy the Odds.
+            <br />
+            Command your fleet - the war for stars begin now!
+          </p>
+          <Button buttonClass="bg-yellow-300" text="Watch Trailer" />
+        </div>
+
+        <div className="absolute bottom-5 right-12">
+          <h2 className="hero-heading special-font text-blue-75">
+            G<b>a</b>ming
+          </h2>
         </div>
       </div>
     </section>
